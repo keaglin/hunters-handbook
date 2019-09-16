dbe0a47eba7e6207054ea50517d8fdfba4589b94
Matthew Cool <matthewbcool@gmail.com>
2019-09-16 07:43:28 -0700

    move search to parent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import Search from './components/Search'
+import React, {useState} from 'react'
 import Details from './components/Details'
 import styled from 'styled-components'
+import algoliasearch from 'algoliasearch/lite'
+import { InstantSearch} from 'react-instantsearch-dom'
+import {HitItemWrapper, SearchWrapper, HitList, SearchInput} from './styles.js'
 
 const TempWrapper = styled.div`
   height: 100vh;
@@ -16,11 +18,41 @@ const TempWrapper = styled.div`
 `
 
 
+const searchClient = algoliasearch(
+  'MT2HPEHTBE',
+  '91b84fdc5b74fbe6ecc908f7738f0e82'
+)
 
-function App() {
+const App = () => {
+  const [toggleDetails, setToggleDetails] = useState(false)
+  const toggleSearch = () => {
+    console.log('toggle search')
+    setToggleDetails(!toggleDetails)
+    console.log(toggleDetails)
+   }
+   const Hit = ({ hit }) => (
+    <HitItemWrapper onClick={toggleSearch} tabIndex='0'>{hit.name}</HitItemWrapper>
+  )
+  
   return (
     <TempWrapper>
-      <Search />
+      <SearchWrapper>
+      <InstantSearch searchClient={searchClient} indexName='dev_MHW-Monsters'>
+        <SearchInput
+          translations={{
+            placeholder: 'Enter Monster...'
+          }}
+          submit={
+            <img
+              src='/img/transparent-arrow.svg'
+              class='submit-search'
+              alt='search monster list'
+            />
+          }
+        />
+        <HitList hitComponent={Hit} />
+      </InstantSearch>
+    </SearchWrapper>
      <Details />
     </TempWrapper>
   )
