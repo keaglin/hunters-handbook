9b722537ffea3ce24f55eed18edfc93c6286415a
Matthew Cool <matthewbcool@gmail.com>
2019-09-23 08:14:49 -0500

    onclick for icon addded

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
-import Details from './Details'
-import styled from 'styled-components'
-import algoliasearch from 'algoliasearch/lite'
-import { InstantSearch } from 'react-instantsearch-dom'
-import {  HitItemWrapper,
-          SearchWrapper,
-          HitList,
-          SearchInput
-        } from '../styles.js'
+import React, { useState } from 'react';
+import Details from './Details';
+import styled from 'styled-components';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch } from 'react-instantsearch-dom';
+import HandBookIcon from './HandBookIcon';
+import {
+  HitItemWrapper,
+  SearchWrapper,
+  HitList,
+  SearchInput
+} from '../styles.js';
 
 const TempWrapper = styled.div`
   height: 100vh;
@@ -19,60 +21,73 @@ const TempWrapper = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center center;
-`
- 
+`;
+
 const searchClient = algoliasearch(
   'MT2HPEHTBE',
   '91b84fdc5b74fbe6ecc908f7738f0e82'
-)
- 
+);
+
 const App = () => {
-  const [toggleDetails, setToggleDetails] = useState(false)
-  const [currentMonsterName, setMonsterName] = useState('')
+  const [toggleDetails, setToggleDetails] = useState(false);
+  const [currentMonsterName, setMonsterName] = useState('');
+  const [toggleEntryIcon, setToggleEntryIcon] = useState(true);
+
   const toggleSearch = () => {
-    setToggleDetails(!toggleDetails)
-  }
+    setToggleDetails(!toggleDetails);
+  };
 
   const handleMonsterClick = currentMonsterName => {
-    setMonsterName(currentMonsterName)
-    toggleSearch()
-  }
+    setMonsterName(currentMonsterName);
+    toggleSearch();
+  };
+
+  const handleIconClick = () => {
+    setToggleEntryIcon(!toggleEntryIcon);
+    console.log(toggleEntryIcon);
+  };
 
   const Hit = ({ hit }) => (
-    <HitItemWrapper onClick={() => handleMonsterClick(hit.name)} tabIndex='0'>
+    <HitItemWrapper onClick={() => handleMonsterClick(hit.name)} tabIndex="0">
       {hit.name}
     </HitItemWrapper>
-  )
+  );
 
   const Search = (
     <SearchWrapper>
-      <InstantSearch searchClient={searchClient} indexName='dev_MHW-Monsters'>
+      <InstantSearch searchClient={searchClient} indexName="dev_MHW-Monsters">
         <SearchInput
           translations={{
             placeholder: 'Enter Monster...'
           }}
           submit={
             <img
-              src='/img/transparent-arrow.svg'
-              class='submit-search'
-              alt='search monster list'
+              src="/img/transparent-arrow.svg"
+              class="submit-search"
+              alt="search monster list"
             />
           }
         />
         <HitList hitComponent={Hit} />
       </InstantSearch>
     </SearchWrapper>
-  )
+  );
+
+  const renderSearch = toggleDetails ? (
+    <Details monsterName={currentMonsterName} toggleSearch={toggleSearch} />
+  ) : (
+    Search
+  );
 
   return (
     <TempWrapper>
-      {toggleDetails ? (
-        <Details monsterName={currentMonsterName} toggleSearch={toggleSearch} />
+      {toggleEntryIcon ? (
+        <HandBookIcon handleClick={handleIconClick} />
       ) : (
-        Search
+        renderSearch
       )}
     </TempWrapper>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/styles.js
+++ src/styles.js
@@ -1,94 +1,94 @@
-import styled from 'styled-components'
-import { SearchBox, Hits } from 'react-instantsearch-dom'
+import styled from 'styled-components';
+import { SearchBox, Hits } from 'react-instantsearch-dom';
 
 // TODO: fonts
 
 export const SearchInput = styled(SearchBox)`
-width: 800px;
-button {
-  border: none;
-  background: rgba(0, 0, 0);
-  opacity: 0.9;
-}
-form {
-  display: flex;
-  background: url('/img/searchoutline.png') no-repeat center 0,
-    url('/img/searchoutline-bottom.png') no-repeat center bottom;
-  margin-bottom: 5px;
-  width: 624px;
-  justify-content: center;
-  align-items: center;
-}
+  width: 800px;
+  button {
+    border: none;
+    background: rgba(0, 0, 0);
+    opacity: 0.9;
+  }
+  form {
+    display: flex;
+    background: url('/img/searchoutline.png') no-repeat center 0,
+      url('/img/searchoutline-bottom.png') no-repeat center bottom;
+    margin-bottom: 5px;
+    width: 624px;
+    justify-content: center;
+    align-items: center;
+  }
 
-input:focus {
-  outline: none;
-}
-input {
-  border: none;
-  color: white;
-  font-size: 1.7rem;
-  padding: 15px 10px 10px 0px;
-  margin: 5px;
-  height: 40px;
-  width: 404px;
-  background: rgba(0, 0, 0);
-  opacity: 0.9;
-}
-.submit-search {
-  height: 30px;
-  width: 30px;
-}
+  input:focus {
+    outline: none;
+  }
+  input {
+    border: none;
+    color: white;
+    font-size: 1.7rem;
+    padding: 15px 10px 10px 0px;
+    margin: 5px;
+    height: 40px;
+    width: 404px;
+    background: rgba(0, 0, 0);
+    opacity: 0.9;
+  }
+  .submit-search {
+    height: 30px;
+    width: 30px;
+  }
 
-input:hover {
-}
+  input:hover {
+  }
 
-li:focus {
-  box-shadow: 1px 0px 0px 3px #2e5d34;
-}
-`
+  li:focus {
+    box-shadow: 1px 0px 0px 3px #2e5d34;
+  }
+`;
 
 export const HitItemWrapper = styled.div`
-display: flex;
-align-items: center;
-height: 45px;
+  display: flex;
+  align-items: center;
+  height: 45px;
 
-width: 615px;
-border-top: dashed 1px slategrey;
-:focus {
-  outline: none;
-}
-:hover {
-  border-top: solid 1px rgba(105, 181, 100, 1);
-  border-bottom: solid 1px rgba(105, 181, 100, 1);
-}
-`
+  width: 615px;
+  border-top: dashed 1px slategrey;
+  :focus {
+    outline: none;
+  }
+  :hover {
+    border-top: solid 1px rgba(105, 181, 100, 1);
+    border-bottom: solid 1px rgba(105, 181, 100, 1);
+  }
+`;
 
 export const HitList = styled(Hits)`
-color: #fafafa;
-margin-left: 5px;
-ul {
-  margin: 0px;
-  padding-left: 0px;
-}
+  color: #fafafa;
+  margin-left: 5px;
+  ul {
+    margin: 0px;
+    padding-left: 0px;
+  }
 
-li {
-  list-style: none;
-  text-shadow: 0 -1px 0 #fff;
-}
-li:hover {
-  background: linear-gradient(
-    to right,
-    rgba(0, 0, 0, 0.6) 0%,
-    rgba(105, 181, 100, 1) 30%,
-    rgba(0, 0, 0, 0.6) 90%
-  );
-  outline: none;
-}
-a {
-  color: inherit;
-  text-decoration: none;
-}
-`
+  li {
+    list-style: none;
+    text-shadow: 0 -1px 0 #fff;
+  }
+  li:hover {
+    background: linear-gradient(
+      to right,
+      rgba(0, 0, 0, 0.6) 0%,
+      rgba(105, 181, 100, 1) 30%,
+      rgba(0, 0, 0, 0.6) 90%
+    );
+    outline: none;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
 
 export const SearchWrapper = styled.div`
 display: flex;
@@ -101,4 +101,15 @@ background: rgba(0, 0, 0);
 opacity: 0.9;
 margin-left: 50px;
 overflow: hidden;
-`
+`;
+
+export const HandBookWrapper = styled.div`
+  border: solid 2px black;
+  height: 60px;
+  width: 60px;
+`;
+
+export const HandBook = styled.img`
+  height: 50px;
+  width: 50px;
+`;
