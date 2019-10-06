f1da7b25157a94a4ea08bfef98d84b829be97bf6
K. Eaglin <kceaglin@gmail.com>
2019-10-06 00:01:43 -0400

    adds additional fields for strategic monster info; also adds images

--- src/components/ConfigPage.js
+++ src/components/ConfigPage.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Config = () => (
-  <p>Config will go here</p>
+  <div>
+    <p>Hunter's Handbook is zero config! Just activate and enjoy!</p>
+    <p>How to use:</p>
+    <ol>
+      <li>Mouse over the player to see the icon.</li>
+      <li>Click the icon to bring up the search box!</li>
+    </ol>
+  </div>
 )
 
 export default Config
